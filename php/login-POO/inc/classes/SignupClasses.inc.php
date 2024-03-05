e7e7ff060e8a0ce3bc025a4701afe3cd19ce4932
João Marcos <jmpsantana15@gmail.com>
2024-03-04 21:58:33 -0300

    Comentarios

--- php/login-POO/inc/classes/SignupClasses.inc.php
+++ php/login-POO/inc/classes/SignupClasses.inc.php
@@ -1,43 +1,43 @@
 <?php
-require_once "../connection.inc.php";
+require_once "../connection.inc.php";// Call the conection archive
 
 class SignupUser
 {
-    private string $username;
-    private string $email;
-    private string $password;
+    private string $username;//Username propertie
+    private string $email;//Email proppertie
+    private string $password;//Password proppertie
 
-    public function __construct(string $username, string $email, string $password)
+    public function __construct(string $username, string $email, string $password)//Initializes the object with user-provided data.
     {
-        $this->username = $username;
+        $this->username = $username;//Propertie recive data from user
         $this->email = $email;
         $this->password = $password;
     }
 
-    private function HashPw() : string
+    private function HashPw() : string //Generates a secure password hash
     {
-        $options = ['cost' => 12];
+        $options = ['cost' => 12];//Level up the value to difficult the brute force
 
-        return password_hash($this->password, PASSWORD_DEFAULT, $options);
+        return password_hash($this->password, PASSWORD_DEFAULT, $options);//Return the hashed pw for later use
     }
 
-    private function PushToDB(object $pdo, string $username, string $email, string $pw)
+    private function PushToDB(object $pdo, string $username, string $email, string $pw)//Inserts user data into the database.
     {
-        $query = "INSERT INTO users (username, email, password) VALUE (:username, :email, :password)";
+        $query = "INSERT INTO users (username, email, password) VALUE (:username, :email, :password)";//Save the SQL query into a propertie
 
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(":username", $username);
+        $stmt = $pdo->prepare($query);//Prepare the query
+        $stmt->bindParam(":username", $username);//Use the properties to fill the placeholders of the query
         $stmt->bindParam(":email", $email);
-        $stmt->bindParam(":password", $pw);
+        $stmt->bindParam(":password", $pw);//Here we use the hashed PW
 
-        $stmt->execute();
+        $stmt->execute();//Execute the query inside the SQL
     }
 
-    public function CallIt(object $pdo)
+    public function SaveToDB(object $pdo)//Public call to the 'PushToDB' method for more secure
     {
-        $this->PushToDB($pdo, $this->username, $this->email, $this->HashPw());
+        $this->PushToDB($pdo, $this->username, $this->email, $this->HashPw());//Properties use as parameters
     }
 }
 
 $test = new SignupUser("João", "joo@gmail.com", "1234");
-$test->CallIt($pdo);
+$test->SaveToDB($pdo);
